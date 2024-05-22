d86b0ca924a34df13ab6107523f346183769af0c
daiki-24 <>
2024-05-22 12:01:02 +0900

    記事に紐づいたコメントの表示

--- src/Blog.js
+++ src/Blog.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import './Blog.css';
 import Comment from './Comment';
 
 function Blog() {
     let [data, setData] = useState([]);
+
     useEffect(() => {
         console.log("call useEffect START");
         fetch('http://localhost:8080/blog').then(res => {
@@ -20,14 +22,14 @@ function Blog() {
     }, []);
 
     return (
-        <div>
+        <div id='Blog'>
             {data.map((item, index) => (
-                <div key={index}>
+                <div key={index} id='Blog_line'>
                     <h2>{item.title}</h2>
                     <p>{item.article}</p>
+                    <Comment blog={item} />
                 </div>
             ))}
-            <Comment />
         </div>
     );
 }
--- src/Comment.js
+++ src/Comment.js
@@ -1,9 +1,35 @@
 import React, { useState, useEffect } from 'react';
+import './Comment.css'
 
-function Comment() {
+function Comment(blog) {
+    let [data, setData] = useState([]);
+
+    useEffect(() => {
+        console.log("call useEffect START");
+        const path = 'http://localhost:8080/blog/comment/' + blog.blog.id;
+        console.log(blog);
+        console.log("PATH: " + path);
+        fetch(path).then(res => {
+            res.json().then(value => {
+                console.log(value);
+                setData(value);
+            })
+        }).catch(error => {
+            console.log(error);
+            setData([]);
+        });
+
+        console.log("call useEffect END");
+        return () => { };
+    }, []);
+
     return (
-        <div>
-            <h2>コメント一覧</h2>
+        <div id='Comment'>
+            {data.map((item, index) => (
+                <div key={index} id='Comment_line'>
+                    <p>{item.name}:{item.article}</p>
+                </div>
+            ))}
         </div>
     );
 }
