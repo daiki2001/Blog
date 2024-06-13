79e114ee05146ab1908e4ba9fa3e27a77cd90655
daiki-24 <daiki.eguchi@172.16.2.68>
2024-06-13 14:08:12 +0900

    react-router-domの導入

--- React/src/App.js
+++ React/src/App.js
@@ -1,13 +1,38 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import Home from './Home';
 import AllBlog from './AllBlog';
 import Blog from './Blog';
 
 function App() {
+    // ヘッダー
+    const Header = () => {
+        return (
+            <header>
+                <h1>自由気ままにエンジニアリング</h1>
+                {/* ナビゲーションバー */}
+                <nav>
+                    <ul>
+                        <li><Link to="/">ホーム</Link></li>
+                        <li><Link to="/Blog">ブログトップ</Link></li>
+                    </ul>
+                </nav>
+            </header>
+        )
+    }
+
     return (
-        <div className="App">
-            {/* <AllBlog /> */}
-            <Blog blog={1} />
-        </div>
+        <Router>
+            <div className="App">
+                {Header()}
+                {/* 各ページへのルート設定 */}
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/Blog" element={<AllBlog />} />
+                </Routes>
+                {/* <Blog blog={1} /> */}
+            </div>
+        </Router>
     );
 }
 
